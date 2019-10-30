c4e152195b4db556122dc3c70591b40cdac35508
Alessio Lombardi <alessio.lombardi@burohappold.com>
2019-10-30 12:27:50 +0000

    First commit

---
+++ ACORN_Engine/Convert/ToBHoM/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Structure.Elements;
+
+namespace BH.Engine.ACORN
+{
+    public static partial class Convert
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        //Add methods for converting to BHoM from the specific software types. 
+        //Only do this if possible to do without any com-calls or similar to the adapter
+        //Example:
+        //public static Node ToBHoM(this ACORN.Node node)
+        //{
+        //    //Insert code for convertion
+        //}
+
+        /***************************************************/
+    }
+}
